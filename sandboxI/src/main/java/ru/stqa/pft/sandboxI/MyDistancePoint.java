2fd05ef2152b6ebce9604a8470bcf710af66bcb3
Kseniya.Isakova <isakova.kse@gmail.com>
2016-05-23 11:49:21 +0300

    Добавлены тесты и метод

--- sandboxI/src/main/java/ru/stqa/pft/sandboxI/MyDistancePoint.java
+++ sandboxI/src/main/java/ru/stqa/pft/sandboxI/MyDistancePoint.java
@@ -8,23 +8,11 @@ public class MyDistancePoint {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
+    Point p1 = new Point(3.0, 0.0);
 
-     p1.x = 3.1;
-     p1.y = -0.2;
+    Point p2 = new Point(0.0, 4.0);
 
-    Point p2 = new Point();
-
-     p2.x = 0.7;
-     p2.y = -4.9;
-
-    System.out.println("Расстояние между точками с координатами " + "(" + p1.x + ";" + p1.y + ") " + "и " + "(" + p2.x + ";" + p2.y + ") " + "равно " + distance(p1, p2));
-
-  }
-
-  public static double distance(Point p1, Point p2) {
-
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    System.out.println("Расстояние между точками с координатами " + "(" + p1.x + ";" + p1.y + ") " + "и " + "(" + p2.x + ";" + p2.y + ") " + "равно "  + p1.distance(p1, p2));
 
   }
 
---
+++ sandboxI/src/test/java/ru/stqa/pft/sandboxI/DistanceTests.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.sandboxI;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Kseniya.Isakova on 22.05.2016.
+ */
+public class DistanceTests {
+
+  @Test
+  public void testDistance1() {
+    Point p1 = new Point(0.0,0.0);
+    Point p2 = new Point(3.0,0.0);
+    Assert.assertEquals(p1.distance(p1, p2), 3.0);
+  }
+
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(0.0,0.0);
+    Point p2 = new Point(0.0,4.0);
+    Assert.assertEquals(p1.distance(p1, p2), 4.0);
+  }
+
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(3.0,0.0);
+    Assert.assertEquals(p1.distance(p1, new Point (0.0,4.0)), 5.0);
+  }
+
+}
